da7d74eec0ea551e12a7bdb4f2e8a61a41e82d31
Mercy Bickell <mercycat@fb.com>
2020-06-30 15:04:48 -0600

    add view controller for composing tweets

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,40 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Mercy Bickell on 6/30/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *composeTweetTextView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *closeComposeButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *finishTweetButton;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (IBAction)dismissComposeView:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
