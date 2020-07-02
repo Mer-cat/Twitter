e1f7b6997342754a25f4c536bded60d8579c8554
Mercy Bickell <mercycat@fb.com>
2020-07-02 17:40:31 -0600

    add character limit counter to compose view

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -12,10 +12,12 @@
 /**
  * View controller where users may type out a tweet, either as a reply to another tweet or as a new tweet
  */
-@interface ComposeViewController ()
+@interface ComposeViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *composeTweetTextView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *closeComposeButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *finishTweetButton;
+@property (weak, nonatomic) IBOutlet UILabel *charactersRemainingLabel;
+@property (nonatomic) NSInteger characterLimit;
 
 @end
 
@@ -23,7 +25,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.composeTweetTextView.delegate = self;
+    
+    self.characterLimit = 280;
+    //[self setCharactersRemaining];
+    self.charactersRemainingLabel.text = [NSString stringWithFormat:@"%ld", self.characterLimit];
+
 }
+
+//- (void)setCharactersRemaining {
+//    NSInteger charactersRemaining = self.characterLimit - self.composeTweetTextView.text.length;
+//    self.charactersRemainingLabel.text = [NSString stringWithFormat:@"%lu/%ld", charactersRemaining, self.characterLimit];
+//}
 
 // Cancel tweet or reply upon close button press
 - (IBAction)didTapCloseComposeView:(id)sender {
@@ -58,6 +72,29 @@
             }
         }];
     }
+}
+
+/**
+ * Prevents users from exceeding the 280 character limit on tweets
+ * Method is called every time the user edits the text view
+ */
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    
+    // Construct what the new text would be if user's latest edit is allowed
+    NSString *newText = [self.composeTweetTextView.text stringByReplacingCharactersInRange:range withString:text];
+    
+    // Update Character Count Label
+    NSUInteger charactersRemaining = self.characterLimit - newText.length;
+    
+    // Quick fix for overflow issues
+    if(charactersRemaining < 0 || charactersRemaining > 280) {
+        charactersRemaining = 0;
+    }
+    
+    self.charactersRemainingLabel.text = [NSString stringWithFormat:@"%lu/%ld", charactersRemaining, self.characterLimit];;
+    
+    // Returns YES as long as user has not exceeded character limit
+    return newText.length <= self.characterLimit;
 }
 
 @end
